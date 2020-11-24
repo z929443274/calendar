4cff64b9c8d50b51ae6cfc17defb45cee8f1c0cf
Alan <929443274@qq.com>
2020-11-24 15:52:18 +0800

    更改暴露方式

--- src/MyPlugin/index.js
+++ src/MyPlugin/index.js
@@ -1,13 +1,7 @@
-import yhCalendar from './YhCalendar/index';
+import yhCalendar from './YhCalendar.vue';
 
-function install(Vue) {
-  if (install.installed) return;
-  install.installed = true;
-  // 将包装好的yhCalendar 挂到Vue的原型上，作为Vue实例上方法
-  // eslint-disable-next-line no-param-reassign
-  Vue.prototype.$yhCalendar = yhCalendar;
-}
+yhCalendar.install = Vue => Vue.component(yhCalendar.name, yhCalendar)
 
 export default {
-  install,
+  yhCalendar,
 };
